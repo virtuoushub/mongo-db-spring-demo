b09781736f5a6f46960f3df9489aefb67023ae7c
Peter Colapietro <pcolapietro@virtusa.com>
2014-11-17 15:33:13 -0500

    Added in configuration annotations.

--- src/main/java/com/virtusa/spring/demo/Application.java
+++ src/main/java/com/virtusa/spring/demo/Application.java
@@ -1,14 +1,37 @@
 package com.virtusa.spring.demo;
 
+import bookmarks.Account;
+import bookmarks.AccountRepository;
+import bookmarks.Bookmark;
+import bookmarks.BookmarkRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.orm.jpa.EntityScan;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+import java.util.Arrays;
 
 @Configuration
 @ComponentScan
 @EnableAutoConfiguration
+@EntityScan(basePackageClasses = {Account.class, Bookmark.class})
+@EnableJpaRepositories(basePackageClasses = {AccountRepository.class, BookmarkRepository.class})
 public class Application {
+
+
+    @Bean
+    CommandLineRunner init(AccountRepository accountRepository, BookmarkRepository bookmarkRepository) {
+        return (evt) ->
+                Arrays.asList("jhoeller", "dsyer", "pwebb", "ogierke", "rwinch", "mfisher", "mpollack").forEach(a -> {
+                    Account account = accountRepository.save(new Account(a, "password"));
+                    bookmarkRepository.save(new Bookmark(account, "http://bookmark.com/1/" + a, "A description"));
+                    bookmarkRepository.save(new Bookmark(account, "http://bookmark.com/2/" + a, "A description"));
+                });
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
